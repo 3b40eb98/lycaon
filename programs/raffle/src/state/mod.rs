f93dede19440788aab976081408334d13a04e157
Wosley Vacemberg <wosleyv@gmail.com>
2022-06-29 13:22:41 -0300

    export state vault

--- programs/raffle/src/state/mod.rs
+++ programs/raffle/src/state/mod.rs
@@ -2,8 +2,10 @@ pub mod bank;
 pub mod entrants;
 pub mod raffle;
 pub mod tickets;
+pub mod vault;
 
 pub use bank::*;
 pub use entrants::*;
 pub use raffle::*;
 pub use tickets::*;
+pub use vault::*;
