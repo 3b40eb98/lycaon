94b9e8763769e1f3d8c8f62e8f7973d7b12065b7
Wosley Vacemberg <wosleyv@gmail.com>
2022-06-29 13:21:36 -0300

    remove unused impl

--- programs/raffle/src/instructions/init_vault.rs
+++ programs/raffle/src/instructions/init_vault.rs
@@ -1,30 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
-use spl_token::instruction::AuthorityType;
 
 use crate::state::*;
-
-// impl<'info> InitVault<'info> {
-//   fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-//     let cpi_accounts = Transfer {
-//       from: self
-//         .initializer_deposit_token_account
-//         .to_account_info()
-//         .clone(),
-//       to: self.vault_account.to_account_info().clone(),
-//       authority: self.initializer.clone(),
-//     };
-//     CpiContext::new(self.token_program.clone(), cpi_accounts)
-//   }
-
-//   fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
-//     let cpi_accounts = SetAuthority {
-//       account_or_mint: self.vault_account.to_account_info().clone(),
-//       current_authority: self.initializer.clone(),
-//     };
-//     CpiContext::new(self.token_program.clone(), cpi_accounts)
-//   }
-// }
 
 pub fn handler(ctx: Context<InitVault>) -> Result<()> {
   let vault = &mut ctx.accounts.vault;
@@ -38,12 +14,6 @@ pub fn handler(ctx: Context<InitVault>) -> Result<()> {
   vault.authority = vault_authority;
   vault.authority_seed = payer_addr;
   vault.authority_bump_seed = [vault_authority_bump];
-
-  // token::set_authority(
-  //   ctx.accounts.into_set_authority_context(),
-  //   AuthorityType::AccountOwner,
-  //   Some(vault_authority),
-  // )?;
 
   Ok(())
 }
